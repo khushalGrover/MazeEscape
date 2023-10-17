8a0346a4e5c1e3c7e1c6f60a89d5de8c4f12155e
khushalGrover <khushal15Grover@gmail.com>
2023-10-18 03:09:52 +0530

    both joystick working
    movement input joystick and camera rotation joystick working fine

--- MazeEscapeProj/Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
+++ MazeEscapeProj/Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
@@ -11,15 +11,18 @@ namespace KinematicCharacterController.Examples
         public ExampleCharacterController Character;
         public ExampleCharacterCamera CharacterCamera;
 
-        private const string MouseXInput = "Mouse X";
-        private const string MouseYInput = "Mouse Y";
+
+        [SerializeField] private Joystick joystickRotation;
+        [SerializeField] private Joystick joystickMovement;
+        // private const string MouseXInput = "Mouse X";
+        // private const string MouseYInput = "Mouse Y";
         private const string MouseScrollInput = "Mouse ScrollWheel";
         private const string HorizontalInput = "Horizontal";
         private const string VerticalInput = "Vertical";
 
         private void Start()
         {
-            Cursor.lockState = CursorLockMode.Locked;
+            // Cursor.lockState = CursorLockMode.Locked;
 
             // Tell camera to follow transform
             CharacterCamera.SetFollowTransform(Character.CameraFollowPoint);
@@ -33,9 +36,9 @@ namespace KinematicCharacterController.Examples
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Cursor.lockState = CursorLockMode.Locked;
+                // Cursor.lockState = CursorLockMode.Locked;
             }
-
+            
             HandleCharacterInput();
         }
 
@@ -54,16 +57,13 @@ namespace KinematicCharacterController.Examples
         private void HandleCameraInput()
         {
             // Create the look input vector for the camera
-            float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
-            float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
-            Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
+            //float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
+            //float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
+            // Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
 
-            // Prevent moving the camera while the cursor isn't locked
-            if (Cursor.lockState != CursorLockMode.Locked)
-            {
-                lookInputVector = Vector3.zero;
-            }
-
+            // using value from vertual joystick
+            Vector3 lookInputVector = new Vector3 (joystickRotation.Horizontal, joystickRotation.Vertical, 0f);
+            
             // Input for zooming the camera (disabled in WebGL because it can cause problems)
             float scrollInput = -Input.GetAxis(MouseScrollInput);
 #if UNITY_WEBGL
@@ -87,6 +87,9 @@ namespace KinematicCharacterController.Examples
             // Build the CharacterInputs struct
             characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
             characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
+            characterInputs.MoveAxisForward = joystickMovement.Vertical;
+            characterInputs.MoveAxisRight = joystickMovement.Horizontal;
+
             characterInputs.CameraRotation = CharacterCamera.Transform.rotation;
             characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
             characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
